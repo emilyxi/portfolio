8a1794c7c20b88614d211c53eb17e6a3d0db3819
emilyxi <emilydxi03@gmail.com>
2022-11-05 18:49:14 -0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,27 +4,57 @@ module.exports = {
   './pages/**/*.{html,js}',
   './index.html',],
   theme: {
+    
     extend: {
+      screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      },
       colors: {
-        'body': '#121D38',
+        'body': '#294283',
         'green-selected-text': '#A3C8AB',
         'pink-selected-text': '#B26B9F',
         'regular-text': '#FFFFFF',
-        'green': '#A3C8AB',
-        'pink': '#B26B9F'
+        'yellow': '#DEBD69',
+        'green': '#5dbc70',
+        'blue': '#A9E0FF',
+        'pink': '#de5499'
 
       }, 
-      spacing: {
-        '1': '8px',
-        '2': '12px',
-        '3': '16px',
-        '4': '24px',
-        '5': '32px',
-        '6': '48px',
-      },
+    
       fontFamily: {
         'folklore': ['IM FELL DW Pica', 'sans-serif']
-      }
+      },
+      keyframes:{
+        wave: {
+          '0%': { transform: 'rotate(0.0deg)' },
+          '10%': { transform: 'rotate(14deg)' },
+          '20%': { transform: 'rotate(-8deg)' },
+          '30%': { transform: 'rotate(14deg)' },
+          '40%': { transform: 'rotate(-4deg)' },
+          '50%': { transform: 'rotate(10.0deg)' },
+          '60%': { transform: 'rotate(0.0deg)' },
+          '100%': { transform: 'rotate(0.0deg)' },
+        },
+      },
+
+      animation: {
+        'waving-hand': 'wave 2s linear infinite',
+      },
+
+
     },
   },
 }
