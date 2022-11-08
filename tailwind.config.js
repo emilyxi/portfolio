1af8bdd42af78df1d253bba7969ed5b9588aa8be
emilyxi <emilydxi03@gmail.com>
2022-11-07 20:51:34 -0800

    formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,24 +4,26 @@ module.exports = {
   './pages/**/*.{html,js}',
   './index.html',],
   theme: {
+    screens: {
+      'xs': '414px',
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     
     extend: {
-      screens: {
-        'sm': '640px',
-        // => @media (min-width: 640px) { ... }
-  
-        'md': '768px',
-        // => @media (min-width: 768px) { ... }
-  
-        'lg': '1024px',
-        // => @media (min-width: 1024px) { ... }
-  
-        'xl': '1280px',
-        // => @media (min-width: 1280px) { ... }
-  
-        '2xl': '1536px',
-        // => @media (min-width: 1536px) { ... }
-      },
+      
       colors: {
         'body': '#294283',
         'green-selected-text': '#A3C8AB',
